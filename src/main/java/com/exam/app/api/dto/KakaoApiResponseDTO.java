8af2f3440e8930b381dcb858a035ae7a9830b918
LimJiNa <jn4624@gmail.com>
2023-12-09 23:44:55 +0900

    kakao api 연동 구현 - 주소 검색하기

--- src/main/java/com/exam/app/api/dto/KakaoApiResponseDTO.java
+++ src/main/java/com/exam/app/api/dto/KakaoApiResponseDTO.java
@@ -1,5 +1,6 @@
 package com.exam.app.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,8 +12,10 @@ import java.util.List;
 @NoArgsConstructor
 public class KakaoApiResponseDTO {
 
+    @JsonProperty("meta")
     private MetaDTO metaDto;
 
+    @JsonProperty("documents")
     private List<DocumentDTO> documentList;
 
 }
