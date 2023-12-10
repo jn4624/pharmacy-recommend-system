c8be6b46f7ae176892dd3a17c8d901e86a27cf5f
LimJiNa <jn4624@gmail.com>
2023-12-10 20:10:57 +0900

    통합 테스트 코드 추가 - saveAll

--- src/test/groovy/com/exam/app/pharmacy/repository/PharmacyRepositoryTest.groovy
+++ src/test/groovy/com/exam/app/pharmacy/repository/PharmacyRepositoryTest.groovy
@@ -9,6 +9,11 @@ class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
     @Autowired
     private PharmacyRepository pharmacyRepository;
 
+    def setup() {
+        // 데이터베이스 초기화
+        pharmacyRepository.deleteAll()
+    }
+
     def "PharmacyRepository save"() {
         given:
         String address = "서울 특별시 성북구 종암동"
@@ -31,6 +36,28 @@ class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
         result.getPharmacyName() == name
         result.getLongitude() == longitude
         result.getLatitude() == latitude
+    }
+
+    def "PharmacyRepository saveAll"() {
+        given:
+        String address = "서울 특별시 성북구 종암동"
+        String name = "은혜 약국"
+        double longitude = 128.11
+        double latitude = 36.11
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .longitude(longitude)
+                .latitude(latitude)
+                .build()
+
+        when:
+        pharmacyRepository.saveAll(Arrays.asList(pharmacy))
+        def result = pharmacyRepository.findAll()
+
+        then:
+        result.size() == 1
     }
 
 }
