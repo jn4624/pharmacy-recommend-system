368ad3ff81d777f7bbb3c1866339b1e4466aecf9
LimJiNa <jn4624@gmail.com>
2023-12-14 22:26:27 +0900

    spring retry 설정 - api 호출 재처리

--- src/main/java/com/exam/app/api/service/KakaoAddressSearchService.java
+++ src/main/java/com/exam/app/api/service/KakaoAddressSearchService.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.client.RestTemplate;
@@ -24,6 +27,11 @@ public class KakaoAddressSearchService {
     @Value("${kakao.rest.api.key}")
     private String kakaoRestApiKey;
 
+    @Retryable(
+            value = {RuntimeException.class},
+            maxAttempts = 2,
+            backoff = @Backoff(delay = 2000)
+    )
     public KakaoApiResponseDTO requestAddressSearch(String address) {
         if (ObjectUtils.isEmpty(address)) return null;
 
@@ -35,6 +43,12 @@ public class KakaoAddressSearchService {
         HttpEntity httpEntity = new HttpEntity<>(headers);
 
         return restTemplate.exchange(uri, HttpMethod.GET, httpEntity, KakaoApiResponseDTO.class).getBody();
+    }
+
+    @Recover
+    public KakaoApiResponseDTO recover(RuntimeException e, String address) {
+        log.error("All the retries failed. address: {}, error: {}", address, e.getMessage());
+        return null;
     }
 
 }
