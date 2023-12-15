e561fca78c3f3ba58ae298f243362d94b041b81f
LimJiNa <jn4624@gmail.com>
2023-12-16 00:26:12 +0900

    통합 테스트 코드 추가 - spock의 where 블럭 사용하여 주소 검색 서비스 검증

--- src/test/groovy/com/exam/app/api/service/KakaoAddressSearchServiceTest.groovy
+++ src/test/groovy/com/exam/app/api/service/KakaoAddressSearchServiceTest.groovy
@@ -10,7 +10,7 @@ class KakaoAddressSearchServiceTest extends AbstractIntegrationContainerBaseTest
 
     def "address 파라미터값이 null이면, requestAddressSearch 메소드는 null을 리턴한주소값"() {
         given:
-        String address = null;
+        String address = null
 
         when:
         def result = kakaoAddressSearchService.requestAddressSearch(address)
@@ -30,6 +30,31 @@ class KakaoAddressSearchServiceTest extends AbstractIntegrationContainerBaseTest
         result.documentList.size() > 0
         result.metaDto.totalCount > 0
         result.documentList.get(0).addressName != null
+    }
+
+    def "정상적인 주소를 입력했을 경우, 정상적으로 위도와 경로를 반환한다"() {
+        given:
+        boolean actualResult = false
+
+        when:
+        def searchResult = kakaoAddressSearchService.requestAddressSearch(inputAddress)
+
+        then:
+        if (searchResult == null) {
+            actualResult = false
+        } else {
+            actualResult = searchResult.getDocumentList().size() > 0
+        }
+
+        where:
+        inputAddress | expectedResult
+        "서울특별시 성북구 종암동" | true
+        "서울 성북구 종동 91" | true
+        "서울 대학로" | true
+        "서울 성북구 종암동 잘못된 주소" | false
+        "광진구 구의동 251-45" | true
+        "광진구 구의동 251-455555" | false
+        "" | false
     }
 
 }
